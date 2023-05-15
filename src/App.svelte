5a2fc2212687e86ebc722ffc07fabf1fc60cf4d8
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-15 15:35:27 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,25 @@
 <script>
-	import Grid from "gridjs-svelte";
-	
-	
-		const data = [
-	  { name: "Sachin", runs: 320000, age: 40, centuries: 100,  wickets: 168 },
-	  { name: "Kohli", runs: 25000, age: 34, centuries: 75, wickets: 78 },
-	  { name: "Root", runs: 19000, age: 33, centuries: 45, wickets: 98 },
-	  { name: "Smith", runs: 17000, age: 32, centuries: 41, wickets: 119 },
-	  { name: "Pointing", runs: 22000, age: 37, centuries: 65, wickets: 68 },
-	  { name: "Kane", runs: 18000, age: 37, centuries: 35, wickets: 61 },
-	  { name: "Anderson", runs: 2000, age: 39, centuries: 1, wickets: 678 },
-	  { name: "Warne", runs: 1800, age:39, centuries: 2, wickets: 778 },
-	];
-  
-	  
+  import Grid from "gridjs-svelte"
 
-	const columns = ["name", "runs" , "age" , "centuries" , "Wickets"];
-	
-  
+  const data = [
+      { name: "maha", email: "maha@example.com", age: "23", mobileno: "9873366463" },
+      { name: "Manasa", email: "manasa@gmail.com",  age: "21",mobileno: "9876545678"},
+      {name: "giri", email: "giri@gmail.com", age:"18" , mobileno: "8766554324"},
+      { name: "mahesh", email: "mahesh@example.com", age: "22", mobileno: "6873366463" },
+      { name: "kavya", email: "kavya@gmail.com",  age: "21",mobileno: "9876545678"},
+      {name: "ganesh", email: "ganesh@gmail.com", age:"19" , mobileno: "7766554324"},
+      { name: "madhu", email: "madhu@example.com", age: "24", mobileno: "7873366463" },
+      { name: "vicky", email: "vicky@gmail.com",  age: "21",mobileno: "8876545678"},
+      {name: "gagan", email: "gagan@gmail.com", age:"19" , mobileno: "9766554324"}
+  ]
+</script>
 
-  </script>
-  <Grid data={data} 
-   sort 
-   search 
-   pagination={{ enabled: true, limit:5 }}  />
+<Grid 
+sort
+search 
+pagination={{ enabled: true, limit: 5 }}
+data={data}/>
 
-  <style global>
+<style global>
   @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-  </style>
-  
+</style>
